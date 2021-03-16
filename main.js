3ef0bae744cc3ba4600658ae37854b5cf8b3de41
HSC-1 <kreaoo13@gmail.com>
2021-03-16 21:06:20 +0900

    api 수정

--- main.js
+++ main.js
@@ -1,228 +1,290 @@
-(function () {
-	'use strict';
+// common number filters
+Vue.filter( 'toFixed', ( num, asset ) => {
+  if ( typeof asset === 'number' ) return Number( num ).toFixed( asset );
+  return Number( num ).toFixed( ( asset === 'USDT' ) ? 3 : 8 );
+});
+Vue.filter( 'toMoney', num => {
+  return Number( num ).toFixed( 0 ).replace( /./g, ( c, i, a ) => {
+    return i && c !== "." && ( ( a.length - i ) % 3 === 0 ) ? ',' + c : c;
+  });
+});
 
-	let urls    = {
-		'pricemultifull' : '%api_uri%pricemultifull?fsyms=%coin%&tsyms=USD',
-		'historical' : '%api_uri%histohour?fsym=%coin%&tsym=USD&limit=%limit%&aggregate=1&e=CCCAGG',
-	};
-	let options = {
-		api_uri : 'https://min-api.cryptocompare.com/data/', // api url
-		coins : [ 'BTC', 'ETH', 'ZEC', ], // needed coins
-		limit : 24 * 7, // 24 hours for every day in a week
-	};
+// component for creating line chart
+Vue.component( 'linechart', {
+  props: {
+    width: { type: Number, default: 400, required: true },
+    height: { type: Number, default: 40, required: true },
+    values: { type: Array, default: [], required: true },
+  },
+  data() {
+    return { cx: 0, cy: 0 };
+  },
+  computed: {
+    viewBox() {
+      return '0 0 '+ this.width +' '+ this.height;
+    },
+    chartPoints() {
+      let data = this.getPoints();
+      let last = data.length ? data[ data.length - 1 ] : { x: 0, y: 0 };
+      let list = data.map( d => ( d.x - 10 ) +','+ d.y );
+      this.cx  = last.x - 5;
+      this.cy  = last.y;
+      return list.join( ' ' );
+    },
+  },
+  methods: {
+    getPoints() {
+      this.width  = parseFloat( this.width ) || 0;
+      this.height = parseFloat( this.height ) || 0;
+      let min     = this.values.reduce( ( min, val ) => val < min ? val : min, this.values[ 0 ] );
+      let max     = this.values.reduce( ( max, val ) => val > max ? val : max, this.values[ 0 ] );
+      let len     = this.values.length;
+      let half    = this.height / 2;
+      let range   = ( max > min ) ? ( max - min ) : this.height;
+      let gap     = ( len > 1 ) ? ( this.width / ( len - 1 ) ) : 1;
+      let points  = [];
 
-	// global variable for storing template blocks
-	let cache = {};
+      for ( let i = 0; i < len; ++i ) {
+        let d = this.values[ i ];
+        let val = 2 * ( ( d - min ) / range - 0.5 );
+        let x = i * gap;
+        let y = -val * half * 0.8 + half;
+        points.push( { x, y } );
+      }
+      return points;
+    }
+  },
+  template: `
+  <svg :viewBox="viewBox" xmlns="http://www.w3.org/2000/svg">
+    <polyline class="color" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" :points="chartPoints" />
+    <circle class="color" :cx="cx" :cy="cy" r="4" fill="#fff" stroke="none" />
+  </svg>`,
+});
 
-	/**
-	 * Function, that put the data to template block, and return complete HTML.
-	 *
-	 * @param str
-	 * @param data
-	 * @returns {Function}
-	 */
-	function tmpl( str, data ) {
-		// Figure out if we're getting a template, or if we need to
-		// load the template - and be sure to cache the result.
-		let fn = !/\W/.test( str ) ?
-			cache[ str ] = cache[ str ] ||
-				tmpl( document.getElementById( str ).innerHTML ) :
+// vue instance
+new Vue({
+  // mount point
+  el: '#app',
 
-			// Generate a reusable function that will serve as a template
-			// generator (and which will be cached).
-			new Function( "obj",
-				"var p=[],print=function(){p.push.apply(p,arguments);};" +
+  // app data
+  data: {
+    endpoint : 'wss://stream.binance.com:9443/ws/!ticker@arr',
+    iconbase : 'https://raw.githubusercontent.com/rainner/binance-watch/master/public/images/icons/',
+    cache    : {},             // coins data cache
+    coins    : [],             // live coin list from api
+    asset    : 'BTC',          // filter by base asset pair
+    search   : '',             // filter by search string
+    sort     : 'assetVolume',  // sort by param
+    order    : 'desc',         // sort order ( asc, desc )
+    limit    : 50,             // limit list
+    status   : 0,              // socket status ( 0: closed, 1: open, 2: active, -1: error )
+    sock     : null,           // socket inst
+    cx       : 0,
+    cy       : 0,
+  },
 
-				// Introduce the data as local variables using with(){}
-				"with(obj){p.push('" +
+  // computed methods
+  computed: {
 
-				// Convert the template into pure JavaScript
-				str
-				//.toString()
-					.replace( /[\r\t\n]/g, " " )
-					.split( "<%" ).join( "\t" )
-					.replace( /((^|%>)[^\t]*)'/g, "$1\r" )
-					.replace( /\t=(.*?)%>/g, "',$1,'" )
-					.split( "\t" ).join( "');" )
-					.split( "%>" ).join( "p.push('" )
-					.split( "\r" ).join( "\\'" )
-				+ "');}return p.join('');" );
-		// Provide some basic currying to the user
-		return data ? fn( data ) : fn;
-	};
+    // process coins list
+    coinsList() {
+      let list = this.coins.slice();
+      let search = this.search.replace( /[^\s\w\-\.]+/g, '' ).replace( /[\r\s\t\n]+/g, ' ' ).trim();
 
+      if ( this.asset ) {
+        list = list.filter( i => i.asset === this.asset );
+      }
+      if ( search && search.length > 1 ) {
+        let reg = new RegExp( '^('+ search +')', 'i' );
+        list = list.filter( i => reg.test( i.token ) );
+      }
+      if ( this.sort ) {
+        list = this.sortList( list, this.sort, this.order );
+      }
+      if ( this.limit ) {
+        list = list.slice( 0, this.limit );
+      }
+      return list;
+    },
 
-	/**
-	 * Get request.
-	 *
-	 * @param options
-	 * @returns {Promise<any>}
-	 */
-	function get_contents( options ) {
-		return new Promise( function ( resolve, reject ) {
-			let xhr = new XMLHttpRequest();
-			xhr.open( options.method, options.url );
-			xhr.onload  = function () {
-				if ( this.status >= 200 && this.status < 300 ) {
-					resolve( xhr.response );
-				} else {
-					reject( {
-						status : this.status,
-						statusText : xhr.statusText
-					} );
-				}
-			};
-			xhr.onerror = function () {
-				reject( {
-					status : this.status,
-					statusText : xhr.statusText
-				} );
-			};
-			if ( options.headers ) {
-				Object.keys( options.headers ).forEach( function ( key ) {
-					xhr.setRequestHeader( key, options.headers[ key ] );
-				} );
-			}
-			let params = options.params;
-			// We'll need to stringify if we've been given an object
-			// If we have a string, this is skipped.
-			if ( params && typeof params === 'object' ) {
-				params = Object.keys( params ).map( function ( key ) {
-					return encodeURIComponent( key ) + '=' + encodeURIComponent( params[ key ] );
-				} ).join( '&' );
-			}
-			xhr.send( params );
-		} );
-	}
+    // show socket connection loader
+    loaderVisible() {
+      return ( this.status === 2 ) ? false : true;
+    },
 
-	/**
-	 * Request data for week.
-	 *
-	 * @param options
-	 * @param url
-	 */
-	function get__week( url, options ) {
+    // sort-by label for buttons, etc
+    sortLabel() {
+      switch ( this.sort ) {
+        case 'token'       :  return 'Token';
+        case 'percent'     :  return 'Percent';
+        case 'close'       :  return 'Price';
+        case 'change'      :  return 'Change';
+        case 'assetVolume' :  return 'Volume';
+        case 'tokenVolume' :  return 'Volume';
+        case 'trades'      :  return 'Trades';
+        default            :  return 'Default';
+      }
+    },
+  },
 
+  // custom methods
+  methods: {
 
-		for ( let i in options.coins ) {
-			if ( options.coins.hasOwnProperty( i ) ) {
-				let uri  = url;
-				uri      = uri.replace( '%api_uri%', options.api_uri );
-				uri      = uri.replace( '%coin%', options.coins[ i ] );
-				uri      = uri.replace( '%limit%', options.limit );
-				let data = get_contents( {
-					method : 'GET',
-					url : uri
-				} )
-					.then( function ( result ) {
-						//console.log( options.coins[ i ] );
-						//console.log( JSON.parse( result ) );
-					} )
-					.catch( function ( err ) {
-						console.error( 'There was an error!', err.statusText );
-					} );
-			}
-		}
-	}
+    // apply sorting and toggle order
+    sortBy( key, order ) {
+      if ( this.sort !== key ) { this.order = order || 'asc'; }
+      else { this.order = ( this.order === 'asc' ) ? 'desc' : 'asc'; }
+      this.sort = key;
+    },
 
-	function get_pricemultifull_data( url, options ) {
-		let coins = options.coins.join( ',' );
-		let uri   = url;
-		uri       = uri.replace( '%api_uri%', options.api_uri );
-		uri       = uri.replace( '%coin%', coins );
-		//console.log( uri );
-		let data  = get_contents( {
-			method : 'GET',
-			url : uri
-		} )
-			.then( function ( result ) {
+    // filter by asset
+    filterAsset( asset ) {
+      this.asset = String( asset || 'BTC' );
+    },
 
-				let block = document.getElementById( 'coin' );
-				let temp  = document.getElementById( 'js-template__coin' ).innerHTML;
-				let time  = new Date().getTime();
-				time      = 100 * Math.ceil( new Date().getTime() / 100000 ); // remove milliseconds and round
-			                                                                  // hundreds
-				result      = JSON.parse( result );
-				let display = result.DISPLAY;
+    // set list limit
+    setLimit( limit ) {
+      this.limit = parseInt( limit ) || 0;
+    },
 
-				block.innerHTML = '';
+    // on socket connected
+    onSockOpen( e ) {
+      this.status = 1; // open
+      console.info( 'WebSocketInfo:', 'Connection open ('+ this.endpoint +').' );
+    },
 
-				// loop for coins
-				for ( let coin_name in display ) {
+    // on socket closed
+    onSockClose( e ) {
+      this.status = 0; // closed
+      console.info( 'WebSocketInfo:', 'Connection closed ('+ this.endpoint +').' );
+      setTimeout( this.sockInit, 10000 ); // try again
+    },
 
-					// if coin exists
-					if ( display.hasOwnProperty( coin_name ) ) {
-						let link = 'https://images.cryptocompare.com/sparkchart/' + coin_name + '/USD/latest.png?ts=' + time;
-						let coin = display[ coin_name ][ 'USD' ];
+    // on socket error
+    onSockError( err ) {
+      this.status = -1; // error
+      console.error( 'WebSocketError:', err.message || err );
+      setTimeout( this.sockInit, 10000 ); // try again
+    },
 
-						let classes = {
-							'PRICE' : '',
-							'MKTCAP' : '',
-							'CHANGEPCT24HOUR' : '',
-						};
-						console.log( JSON.stringify( coin ) );
+    // process data from socket
+    onSockData( e ) {
+      let list = JSON.parse( e.data ) || [];
 
-						let fields = [ 'PRICE', 'MKTCAP', 'CHANGEPCT24HOUR' ];
+      for ( let item of list ) {
+        // cleanup data for each coin
+        let c = this.getCoinData( item );
+        // keep to up 100 previous close prices in hostiry for each coin
+        c.history = this.cache.hasOwnProperty( c.symbol ) ? this.cache[ c.symbol ].history : this.fakeHistory( c.close );
+        if ( c.history.length > 100 ) c.history = c.history.slice( c.history.length - 100 );
+        c.history.push( c.close );
+        // add coin data to cache
+        this.cache[ c.symbol ] = c;
+      }
+      // convert cache object to final prices list for each symbol
+      this.coins = Object.keys( this.cache ).map( s => this.cache[ s ] );
+      this.status = 2; // active
+    },
 
-						// loop for fields
-						for ( let i in fields ) {
+    // start socket connection
+    sockInit() {
+      if ( this.status > 0 ) return;
+      try {
+        this.status = 0; // closed
+        this.sock = new WebSocket( this.endpoint );
+        this.sock.addEventListener( 'open', this.onSockOpen );
+        this.sock.addEventListener( 'close', this.onSockClose );
+        this.sock.addEventListener( 'error', this.onSockError );
+        this.sock.addEventListener( 'message', this.onSockData );
+      }
+      catch( err ) {
+        console.error( 'WebSocketError:', err.message || err );
+        this.status = -1; // error
+        this.sock = null;
+      }
+    },
 
-							if ( fields.hasOwnProperty( i ) && coins_old_data.hasOwnProperty( coin_name ) ) {
-								let field   = fields[ i ];
-								let current = parseFloat( coin[ field ].replace( /\$|\%/g, '' ) );
+    // start socket connection
+    sockClose() {
+      if ( this.sock ) {
+        this.sock.close();
+      }
+    },
 
-								// if current field isn't CHANGEPCT24HOUR
-								if ( field !== 'CHANGEPCT24HOUR' ) {
-									let old = parseFloat( coins_old_data[ coin_name ][ field ].replace( '$', '' ) );
+    // come up with some fake history prices to fill in the initial line chart
+    fakeHistory( close ) {
+      let num = close * 0.0001; // faction of current price
+      let min = -Math.abs( num );
+      let max = Math.abs( num );
+      let out = [];
 
-									// determine that was rise up or rise down
-									if ( current < old ) {
-										classes[ field ] = 'down';
-									} else if ( current > old ) {
-										classes[ field ] = 'up';
-									} else {
-										classes[ field ] = '';
-									}
-								} else {
-									if ( current > 0 ) {
-										classes[ 'CHANGEPCT24HOUR' ] = 'up';
-									} else if ( current < 0 ) {
-										classes[ 'CHANGEPCT24HOUR' ] = 'down';
-									} else {
-										classes[ 'CHANGEPCT24HOUR' ] = '';
-									}
-								}
-							}
-						}
-						coins_old_data[ coin_name ] = coin;
+      for ( let i = 0; i < 50; ++i ) {
+        let rand = Math.random() * ( max - min ) + min;
+        out.push( close + rand );
+      }
+      return out;
+    },
 
-						block.innerHTML += tmpl( temp, {
-							//'FROMSYMBOL' : coin[ 'FROMSYMBOL' ], // simbol
-							'coin_name' : coin_name, // coin name
-							'PRICE' : coin[ 'PRICE' ], // Price
-							'class_price' : classes[ 'PRICE' ],
-							'MKTCAP' : coin[ 'MKTCAP' ], // Market Cap
-							'class_mktcap' : classes[ 'MKTCAP' ],
-							'TOTALVOLUME24HTO' : coin[ 'TOTALVOLUME24HTO' ], // Direct Vol. 24H
-							'VOLUME24HOURTO' : coin[ 'VOLUME24HOURTO' ], // Direct Vol. 24H
-							'graffic' : link, // graffic
-							'CHANGEPCT24HOUR' : coin[ 'CHANGEPCT24HOUR' ], // Chg. 24H
-							'class_change' : classes[ 'CHANGEPCT24HOUR' ],
-						} );
+    // finalize data for each coin from socket
+    getCoinData( item ) {
+      let reg         = /^([A-Z]+)(BTC|ETH|BNB|USDT|TUSD)$/;
+      let symbol      = String( item.s ).replace( /[^\w\-]+/g, '' ).toUpperCase();
+      let token       = symbol.replace( reg, '$1' );
+      let asset       = symbol.replace( reg, '$2' );
+      let name        = token;
+      let pair        = token +'/'+ asset;
+      let icon        = this.iconbase + token.toLowerCase() + '_.png';
+      let open        = parseFloat( item.o );
+      let high        = parseFloat( item.h );
+      let low         = parseFloat( item.l );
+      let close       = parseFloat( item.c );
+      let change      = parseFloat( item.p );
+      let percent     = parseFloat( item.P );
+      let trades      = parseInt( item.n );
+      let tokenVolume = Math.round( item.v );
+      let assetVolume = Math.round( item.q );
+      let sign        = ( percent >= 0 ) ? '+' : '';
+      let arrow       = ( percent >= 0 ) ? '▲' : '▼';
+      let info        = [ pair, close.toFixed( 8 ), '(', arrow, sign + percent.toFixed( 2 ) +'%', '|', sign + change.toFixed( 8 ), ')' ].join( ' ' );
+      let style       = '';
 
-					}
-				}
-			} )
-			.catch( function ( err ) {
-				console.error( 'There was an error!', err.statusText );
-			} );
+      if ( percent > 0 ) style = 'gain';
+      if ( percent < 0 ) style = 'loss';
 
-	}
+      return { symbol, token, asset, name, pair, icon, open, high, low, close, change, percent, trades, tokenVolume, assetVolume, sign, arrow, style, info };
+    },
 
-	let coins_old_data = [];
-	get_pricemultifull_data( urls[ 'pricemultifull' ], options );
-	setInterval( function () {
-		get_pricemultifull_data( urls[ 'pricemultifull' ], options );
-	}, 3000 );
+    // sort an array by key and order
+    sortList( list, key, order ) {
+      return list.sort( ( a, b ) => {
+        let _a = a[ key ];
+        let _b = b[ key ];
 
-}());
+        if ( _a && _b ) {
+          _a = ( typeof _a === 'string' ) ? _a.toUpperCase() : _a;
+          _b = ( typeof _b === 'string' ) ? _b.toUpperCase() : _b;
+
+          if ( order === 'asc' ) {
+            if ( _a < _b ) return -1;
+            if ( _a > _b ) return 1;
+          }
+          if ( order === 'desc' ) {
+            if ( _a > _b ) return -1;
+            if ( _a < _b ) return 1;
+          }
+        }
+        return 0;
+      });
+    },
+  },
+
+  // app mounted
+  mounted() {
+    this.sockInit();
+  },
+
+  // app destroyed
+  destroyed() {
+    this.sockClose();
+  }
+});
